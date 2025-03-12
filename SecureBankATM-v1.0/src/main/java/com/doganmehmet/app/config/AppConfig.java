c157abd4ee6b17dac3127281a862c5a4ffce76a3
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-03-12 07:01:08 +0100

    update

--- SecureBankATM-v1.0/src/main/java/com/doganmehmet/app/config/AppConfig.java
+++ SecureBankATM-v1.0/src/main/java/com/doganmehmet/app/config/AppConfig.java
@@ -1,13 +1,8 @@
 package com.doganmehmet.app.config;
 
-import com.doganmehmet.app.security.CustomAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.List;
 
 @Configuration
 public class AppConfig {
@@ -17,23 +12,4 @@ public class AppConfig {
     {
         return new BCryptPasswordEncoder();
     }
-
-//    private final IUserRepository m_userRepository;
-//    @Bean
-//    public UserDetailsService userDetailsService()
-//    {
-//        return username -> m_userRepository.findByUsername(username)
-//                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-//    }
-//
-//
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager()
-//    {
-//        var authProvider = new DaoAuthenticationProvider();
-//        authProvider.setUserDetailsService(userDetailsService());
-//        authProvider.setPasswordEncoder(passwordEncoder());
-//        return new ProviderManager(authProvider);
-//    }
 }
